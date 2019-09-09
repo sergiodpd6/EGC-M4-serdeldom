96903c65af1052816e4abf32f7d5ae6b2e0551d6
sergiodpd <sergiodpd6@gmail.com>
2019-09-09 10:53:06 +0200

    Despliegue heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -68,7 +68,7 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://egcserdeldom.herokuapp.com/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -153,10 +153,15 @@ STATIC_URL = '/static/'
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
 
+APIS = {}
+
 try:
     from local_settings import *
 except ImportError:
     print("local_settings.py not found")
+
+import django_heroku
+django_heroku.settings(locals())
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
